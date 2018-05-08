59be6464440ddb5a6c3f6b8e58b737371d5ef960
AndriyPytel <andriypytel@gmail.com>
2018-05-08 14:07:10 +0300

    connecting to server try 7

--- listener.py
+++ listener.py
@@ -3,21 +3,19 @@ import socket
 def main():
     sock = socket.socket()
     sock.bind(('', 9090))
-    sock.listen(1)
+    sock.listen(5)
     conn, addr = sock.accept()
 
     print('connected:', addr)
-    i = 0
     while True:
         data = conn.recv(1024)
+
         if not data:
             break
-        if i == 100:
-            i = 0
-            print("some log")
+
         udata = data.decode("utf-8")
         conn.send(data.upper())
         print(udata)
-        i+=1
+
 
     conn.close()
