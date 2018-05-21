6e168eae09c7cb864db6360e972d81b243615839
AndriyPytel <andriypytel@gmail.com>
2018-05-22 02:50:08 +0300

    add pushing to db try 12

--- db_controler.py
+++ db_controler.py
@@ -1,11 +1,10 @@
 
-def main():
-    pass
 
-def push(data):
-    f = open('test_data.txt', 'a+')
-    f.write(data)
-    f.close()
+db= MySQLdb.connect("localhost", "meteouser", "kwZuq7b3", "meteo")
 
+cursor= db.cursor()
 
+cursor.execute("INSERT INTO first (temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES (t1,h1,t2,h2,p,w)")
+
+db.close()
 
--- listener.py
+++ listener.py
@@ -1,5 +1,8 @@
 import socket
 from db_controler import push
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
 
 
 def main():
@@ -18,9 +21,20 @@ def main():
                 break
 
             udata = data.decode("utf-8")
+            arr = str(udata).split()
             conn.send(data.upper())
             print(udata)
-            push(udata)
+            push(arr[-7],arr[-6],arr[-5],arr[-4],arr[-3],arr[-2],arr[-1])
 
 
         conn.close()
+def push(t1,h1,t2,h2,p,w):
+    print(t1," ",h1," ",t2," ",h2," ",p," ",w)
+    db = MySQLdb.connect("localhost", "meteouser", "kwZuq7b3", "meteo")
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "INSERT INTO first (temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES (t1,h1,t2,h2,p,w)")
+
+    db.close()
