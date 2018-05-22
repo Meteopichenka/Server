c1afb9b080a5aacec9fc35dc30c4e2233fb4b0c4
AndriyPytel <andriypytel@gmail.com>
2018-05-22 07:34:31 +0300

    add new site

--- listener.py
+++ listener.py
@@ -1,4 +1,6 @@
 import socket
+import MySQLdb
+
 
 def main():
     sock = socket.socket()
@@ -20,11 +22,22 @@ def main():
             print(udata)
             arr = str(udata).split()
             v = False
+            res = []
             for i in arr:
                 if v:
-                    print(i)
+                    res.append(i)
                 if i == "Value:":
                     v = True
+            push(res[0],res[1],res[2],res[3],res[5],0)
+
 
 
         conn.close()
+
+def push(t1, h1, t1, h2, p, w):
+    conn = MySQLdb.connect('localhost', 'username', 'password', 'table_name')
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO table_name(temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES(t1, h1, t1, h2, p, w);")
+
+    conn.close()
