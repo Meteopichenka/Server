903ede9c32f804f07983d398da867509e57338cf
AndriyPytel <andriypytel@gmail.com>
2018-05-22 03:27:35 +0300

    add pushing try 3

--- listener.py
+++ listener.py
@@ -1,8 +1,4 @@
 import socket
-'''import pymysql
-pymysql.install_as_MySQLdb()
-import MySQLdb
-'''
 
 def main():
     sock = socket.socket()
@@ -20,20 +16,7 @@ def main():
                 break
 
             udata = data.decode("utf-8")
-            arr = str(udata).split()
             conn.send(data.upper())
             print(udata)
-            push(arr[-7],arr[-6],arr[-5],arr[-4],arr[-3],arr[-2],arr[-1])
-
 
         conn.close()
-def push(t1,h1,t2,h2,p,w):
-    print(t1," ",h1," ",t2," ",h2," ",p," ",w)
-    '''db = MySQLdb.connect("localhost", "meteouser", "kwZuq7b3", "meteo")
-
-    cursor = db.cursor()
-
-    cursor.execute(
-        "INSERT INTO first (temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES (t1,h1,t2,h2,p,w)")
-
-    db.close()'''
