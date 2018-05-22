8a9f95281cae1951922b118f44b06b44eff7cfdf
AndriyPytel <andriypytel@gmail.com>
2018-05-22 07:45:01 +0300

    add new site

--- listener.py
+++ listener.py
@@ -45,6 +45,6 @@ def push(t1, h1, t2, h2, p, w):
     conn = MySQLdb.connect('localhost', 'meteouser', 'kwZuq7b3', 'meteo')
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO table_name(temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES(t1, h1, t2, h2, p, w);")
+    cursor.execute("INSERT INTO first(temperature1, humidity1, temperature2, humidity2, pressure, wind_dir) VALUES(t1, h1, t2, h2, p, w);")
 
     conn.close()
