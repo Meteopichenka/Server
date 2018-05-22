1f8d191d4cd1e583b462805651f87cfe9614f4b6
AndriyPytel <andriypytel@gmail.com>
2018-05-22 04:05:33 +0300

    try to push try 5

--- listener.py
+++ listener.py
@@ -21,9 +21,5 @@ def main():
             arr = str(udata).split()
             l = len(arr)
             print(arr)
-            print(arr[l-1])
-            print(arr[l-2])
-            print(arr[l-3])
-            print(arr[l-4])
-            print(arr[l-5])
+            
         conn.close()
