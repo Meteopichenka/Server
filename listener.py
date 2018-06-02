dd99ec9f0deb9401e64dda8ce3afb6d9a213e670
AndriyPytel <andriypytel@gmail.com>
2018-06-02 18:11:01 +0300

    forecast fix1

--- listener.py
+++ listener.py
@@ -50,7 +50,7 @@ def check_push(d =[]):
     global last_pressure
     if d[0] != 0 and d[2] != 0:
         today = datetime.date.today()
-        forecast = Zambretti.ZambrettiCode(int(d[4]), today.month, d[5], (pressureTrend((last_pressure), float(d[4]))))
+        forecast = Zambretti.ZambrettiCode(int(round(d[4])), today.month, d[5], (pressureTrend((last_pressure), float(d[4]))))
         d.append(forecast)
         print(d)
 
