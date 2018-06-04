043bf62e7dc9b9f55032d0d71f4ce17f2beaf3f2
AndriyPytel <andriypytel@gmail.com>
2018-06-04 09:11:15 +0300

    forecast fix1

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ def http_server_thread():
 def server_listener_thread():
     listener()
     pass
-def php_run():
-    subprocess.call("materials/alpha.php")
+def info_run():
+    subprocess.call("php materials/alpha.php")
+    pass
 
-    # if you want output
-    proc = subprocess.Popen("materials/alpha.php", shell=True, stdout=subprocess.PIPE)
-    script_response = proc.stdout.read()
 
 http_server_thread = threading.Thread(target=http_server_thread, name="http_server_thread")
 server_listener_thread = threading.Thread(target=server_listener_thread, name="server_listener_thread")
-php_run_thread = threading.Thread(target=php_run, name="php_run")
+info_run_thread = threading.Thread(target=info_run, name="php_run")
 
 http_server_thread.start()
 server_listener_thread.start()
-php_run_thread.start()
+info_run_thread.start()
